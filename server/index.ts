c828497e9e4cf1e26c905dbee3126bea3c8da6a2
Jonatan Sidén <jonatan.siden@gmail.com>
2026-03-19 01:03:42 +0100

    fix: always listen on 0.0.0.0 for Docker compatibility

--- server/index.ts
+++ server/index.ts
@@ -96,7 +96,7 @@ app.use((req, res, next) => {
   httpServer.listen(
     {
       port,
-      host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1",
+      host: "0.0.0.0", // 🔥 viktigt
     },
     () => {
       console.log(`🚀 Server running on port ${port}`);
